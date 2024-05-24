1272b02087fe731e0fe6d05a0b78d15009f8140c
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-05-24 17:23:42 -0300

    Category and Product models

---
+++ Models/Category.cs
@@ -0,0 +1,6 @@
+﻿namespace Blazor8Shop.Models; 
+
+public class Category {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor8Shop.Models;
+
+public class Product {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public double Price { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+}
