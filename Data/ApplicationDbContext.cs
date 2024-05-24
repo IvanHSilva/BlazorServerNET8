d645e7ad357e235474a103fe838a71b55c20974f
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-05-24 17:45:47 -0300

    DataContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Blazor8Shop.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,4 +6,6 @@ namespace Blazor8Shop.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
 }
