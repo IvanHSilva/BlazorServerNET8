05229a8fd91124645d13ac56733a5811c638ccc2
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-05-24 17:26:34 -0300

    DataAnnotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,11 @@
-﻿namespace Blazor8Shop.Models; 
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor8Shop.Models; 
 
 public class Category {
+    
+    [Key]
     public int Id { get; set; }
+    [Required(ErrorMessage = "Nome obrigatório")]
     public string Name { get; set; } = string.Empty;
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,9 +1,15 @@
-﻿namespace Blazor8Shop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor8Shop.Models;
 
 public class Product {
+
+    [Key]
     public int Id { get; set; }
+    [Required(ErrorMessage = "Nome obrigatório")] 
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Preço obrigatório")] 
     public double Price { get; set; }
 
     public int CategoryId { get; set; }
